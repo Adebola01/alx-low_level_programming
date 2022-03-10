742234619734e3b21c93ce0f0dd22e88ba05ddfa
Adebola01 <sulieman.adebola@gmail.com>
2022-03-10 05:20:22 -0800

    prints exactly Programming is like building a multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,15 @@
 #include <stdio.h>
-
-
-
 /**
  *
- * * main - print the string in the put function
+ *  * main - Prints quote
  *
- * *
+ *   *
  *
- * * Description: using the main function
+ *    * Return: zero on success
  *
- * * this program prints "Programming is like building a multilingual puzzle
+ *     *
  *
- * * Return: 0
- *
- * */
+ *      */
 
 int main(void)
 
